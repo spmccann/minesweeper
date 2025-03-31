85da38c1a061b4b53293d31732b4833742225805
spmccann <scottpmccann@gmail.com>
2025-03-30 18:51:37 -0700

    feat: images accessible in slice from sprite sheet

--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image"
 	"image/color"
 	_ "image/png"
 	"log"
 )
 
-var img *ebiten.Image
+var sprites *ebiten.Image
+var tileImages []*ebiten.Image
 
 func init() {
 	var err error
-	img, _, err = ebitenutil.NewImageFromFile("assets/sprite_sheet.png")
+	sprites, _, err = ebitenutil.NewImageFromFile("assets/sprite_sheet.png")
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func createTileImages(img *ebiten.Image) {
+	for x := 0; x <= 48; x += 16 {
+		for y := 0; y <= 48; y += 16 {
+			part := img.SubImage(image.Rect(x, y, x+16, y+16))
+			newTile := ebiten.NewImageFromImage(part)
+			tileImages = append(tileImages, newTile)
+		}
 	}
 }
 
@@ -26,7 +38,7 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{52, 40, 145, 1})
-	screen.DrawImage(img, nil)
+	screen.DrawImage(tileImages[0], nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -34,8 +46,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	createTileImages(sprites)
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Minesweeer")
+	ebiten.SetWindowTitle("Minesweeper")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
